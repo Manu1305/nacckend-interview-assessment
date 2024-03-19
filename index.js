876368827b1ac7fc4a3798cf4770a6fd7a7ee588
Manu <manukrishnan1305@gmail.com>
2024-03-19 15:43:30 +0530

    afvfv

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter)
-app.listen(8000, () => {
+app.listen(3000, () => {
     console.log("Server is running on port 8000")
    
     Connectdb()
