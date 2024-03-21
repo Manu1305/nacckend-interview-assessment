2e7f3e29f2dbad44441e31a7216ee8f9a5239420
Manu <manukrishnan1305@gmail.com>
2024-03-21 18:48:53 +0530

    dfvdf

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter)
 app.listen(3000, () => {
-    console.log("Server is running on port 8000")
+    console.log("Server is running on port 3000")
    
     Connectdb()
 })
